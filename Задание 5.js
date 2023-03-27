5930e3859d5a11a920628c91de8e7ca974ae18d6
AStavitskii <pochta100@mail.ru>
2023-03-27 18:56:08 +0300

    task 5 done

---
+++ Задание 5.js
@@ -0,0 +1,11 @@
+const exponentiation = (x, n) => {
+    let result = x;
+  
+    for (let i = 1; i < n; i++) {
+      result *= x;
+    }
+  
+    return result;
+  }
+  
+  console.log(exponentiation(5, 3))
